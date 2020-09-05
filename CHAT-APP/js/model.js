3ffceeda5bf3ebc056eab62b356faa9c4b6418ff
vck2004 <vckkcv2004@gmail.com>
2020-09-05 10:56:03 +0700

    chatApp

--- CHAT-APP/js/model.js
+++ CHAT-APP/js/model.js
@@ -1,5 +1,9 @@
 const model = {}
 
+model.currentUser = undefined
+model.conversations = []
+model.currentConversation = undefined
+
 model.register = async (data) => {
     try {
         const response = await firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
@@ -31,9 +35,47 @@ model.login = (data) => {
     }
 }
 
-model.getFirestoreConversation = async () => {
-    const docId = 'fJzYDPfRARBjfzfUkGBh'
-    const response = await firebase.firestore().collection('conversations').doc(docId).get()
-    const conversation = getOneDocument(response)
-    return conversation
+model.getConversations = async () => {
+    const response = await firebase.firestore().collection('conversations').where('users', 'array-contains', model.currentUser.email).get()
+    model.conversations = getManyDocument(response)
+    if (model.conversations.length > 0) {
+        model.currentConversation = model.conversations[0]
+        view.showCurrentConversation()
+        view.showConversations()
+    }
+}
+
+model.addMessage = (message) => {
+    dataToUpdate = {
+        messages: firebase.firestore.FieldValue.arrayUnion(message)
+    }
+    firebase.firestore().collection('conversations').doc(model.currentConversation.id).update(dataToUpdate)
+}
+
+model.listenConversationChange = () => {
+    let isFirstRun = true
+    firebase.firestore().collection('conversations').where('users', 'array-contains', model.currentUser.email).onSnapshot((snapshot) => {
+        if (isFirstRun) {
+            isFirstRun = false
+            return
+        }
+        for (oneChange of snapshot.docChanges()) {
+            const docData = getOneDocument(oneChange.doc)
+            if (docData.id === model.currentConversation.id) {
+                model.currentConversation = docData
+                view.addMessage(model.currentConversation.messages[model.currentConversation.messages.length - 1])
+                view.scrollToEndElement()
+            }
+            for (let i = 0; i < model.conversations.length; i++) {
+                if (model.conversations[i].id === docData.id) {
+                    model.conversations[i] = docData
+                }
+            }
+        }
+    })
+}
+
+model.addConversation = (newConversation) => {
+    const dataToCreate = newConversation
+    firebase.firestore().collection('conversations').add(dataToCreate)
 }
--- CHAT-APP/js/view.js
+++ CHAT-APP/js/view.js
@@ -1,5 +1,5 @@
 const view = {}
-view.setActiveScreen = async (screenName) => {
+view.setActiveScreen = (screenName) => {
     switch (screenName) {
         case 'registerPage':
             document.getElementById('app').innerHTML = component.registerPage
@@ -36,22 +36,43 @@ view.setActiveScreen = async (screenName) => {
             break
         case 'chatPage':
             document.getElementById('app').innerHTML = component.chatPage
-            view.loadConversation(await model.getFirestoreConversation())
+            document.getElementById('create_conversation').addEventListener('click',()=>{
+                view.setActiveScreen('createConversationPage')
+            })
             const sendMessageForm = document.getElementById('send_message_form')
             sendMessageForm.addEventListener('submit', (e) => {
                 e.preventDefault()
                 const message = {
                     content: sendMessageForm.message.value,
+                    createdAt: new Date().toISOString(),
                     owner: model.currentUser.email
                 }
-                const messageFromBot = {
-                    content: sendMessageForm.message.value,
-                    owner: 'Bot'
-                }
-                if(sendMessageForm.message.value.trim() !== ''){
-                    view.addMessage(message)
-                    view.addMessage(messageFromBot)
+                if (sendMessageForm.message.value.trim() !== '') {
+                    model.addMessage(message)
                     sendMessageForm.message.value = ''
+                }
+            })
+            model.getConversations()
+            model.listenConversationChange()
+            break
+        case 'createConversationPage':
+            document.getElementById('app').innerHTML = component.createConversationPage
+            document.getElementById('redirect_to_chat').addEventListener('click',()=>{
+                view.setActiveScreen('chatPage')
+            })
+            const createConversationForm = document.getElementById('create_conversation_form')
+            createConversationForm.addEventListener('submit',(e)=>{
+                e.preventDefault()
+                const newConversation = {
+                    createdAt: new Date().toISOString(),
+                    messages: [],
+                    title: createConversationForm.title.value,
+                    users: createConversationForm.email.value.split(' ')
+                }
+                newConversation.users.push(model.currentUser.email)
+                if (createConversationForm.title.value.trim() !== '' && createConversationForm.email.value.trim() !== '') {
+                    model.addConversation(newConversation)
+                    view.setActiveScreen('chatPage')
                 }
             })
             break
@@ -78,9 +99,42 @@ view.addMessage = (message) => {
     document.querySelector('.list_messages').appendChild(messageWrapper)
 }
 
-view.loadConversation = (data) => {
-    document.querySelector('.conversation_title').innerText = data.title
-    for(let message of data.messages){
+view.showCurrentConversation = () => {
+    document.querySelector('.conversation_title').innerText = model.currentConversation.title
+    document.querySelector('.list_messages').innerHTML = ''
+    for (let message of model.currentConversation.messages) {
         view.addMessage(message)
     }
+    view.scrollToEndElement()
+}
+
+view.scrollToEndElement = () => {
+    const element = document.querySelector('.list_messages')
+    element.scrollTop = element.scrollHeight
+}
+
+view.showConversations = () => {
+    for(let conversation of model.conversations){
+        view.addConversation(conversation)
+    }
+}
+
+view.addConversation = (conversation) => {
+    const conversationWrapper = document.createElement('div')
+    conversationWrapper.classList.add('conversation')
+    conversationWrapper.classList.add('cursor_pointer')
+    if(conversation.id === model.currentConversation.id){
+        conversationWrapper.classList.add('current')
+    }
+    conversationWrapper.innerHTML = `
+        <div class="left_conversation_title">${conversation.title}</div>
+        <div class="num_of_user">${conversation.users.length} users</div>
+    `
+    conversationWrapper.addEventListener('click',()=>{
+        model.currentConversation = model.conversations.filter(item => item.id === conversation.id)[0]
+        view.showCurrentConversation()
+        document.querySelector('.conversation.current').classList.remove('current')
+        conversationWrapper.classList.add('current')
+    })
+    document.querySelector('.list_conversations').appendChild(conversationWrapper)
 }
